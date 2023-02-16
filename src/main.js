b490e60b05dfe4f1e4c02f868abe0713bb88125c
melkeades <118681335+melkeades@users.noreply.github.com>
2023-02-17 02:53:38 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
-console.log('vcvd')
+// import './styles/style.styl'
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+// import CSSRulePlugin from 'gsapsplit-type/CSSRulePlugin'
+// import CustomEase from 'gsap/CustomEase'
+// import ScrollToPlugin from 'gsap/ScrollToPlugin '
+import ScrollTrigger from 'gsap/ScrollTrigger '
+import $ from 'jquery'
+import SplitType from 'split-type'
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1.3,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)(
+  // faq -------------------------------------------
+  (function ($) {
+    //    'use strict';
+
+    $('.accordion__title').on('click', function () {
+      $(this).children('.accordion__arrow').toggleClass('rotate')
+      $('.accordion__arrow').not($(this).children('.accordion__arrow')).removeClass('rotate')
+      $(this).next().slideToggle(800)
+      $('.accordion__text').not($(this).next()).slideUp('fast')
+
+      // $(this).offset().top-"distance from top"}, "scroll speed");}, "animation delay depending on inner content length");
+      setTimeout(() => {
+        $('html').animate({ scrollTop: $(this).offset().top - 180 }, 800)
+      }, 400)
+    })
+  })(jQuery)
+)
+
+console.log('vfsv')
